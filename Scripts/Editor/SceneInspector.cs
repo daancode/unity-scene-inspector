00f19367adabbfda7c4cb77afc0a82e87362fc14
Damian Barczyński <kontakt.daan@gmail.com>
2018-12-03 21:37:24 +0100

    Added settings menu.

--- Scripts/Editor/SceneInspector.cs
+++ Scripts/Editor/SceneInspector.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
@@ -10,25 +12,29 @@ namespace Quario.Tools.Editor
     [InitializeOnLoad]
     public class SceneInspector
     {
+        static bool ShowAllScenesFromProject = true;
+
         static SceneInspector()
         {
+            if(!EditorPrefs.HasKey("QuarioToolbox:Inspector:ShowAllScenesFromProject"))
+            {
+                EditorPrefs.SetBool("QuarioToolbox:Inspector:ShowAllScenesFromProject", true);
+            }
+
+            ShowAllScenesFromProject = EditorPrefs.GetBool("QuarioToolbox:Inspector:ShowAllScenesFromProject");
             ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
         }
 
         static void OnToolbarGUI()
         {
             GUILayout.FlexibleSpace();
-
             EditorGUI.BeginDisabledGroup(EditorApplication.isPlaying);
             EditorGUILayout.BeginHorizontal();
-
+            CreateSettingsButton();
             CreatePlayButton();
             CreateSceneChangeButton();
-            
             EditorGUI.EndDisabledGroup();
-
             CreateSceneAddButton();
-
             EditorGUILayout.EndHorizontal();
         }
 
@@ -83,14 +89,7 @@ namespace Quario.Tools.Editor
             if (GUILayout.Button(changeSceneContent, GUILayout.Height(23f)) && !EditorApplication.isPlaying)
             {
                 GenericMenu menu = new GenericMenu();
-
-                var scenes = AssetDatabase.FindAssets("t:Scene");
-                foreach (var t in scenes)
-                {
-                    var path = AssetDatabase.GUIDToAssetPath(t);
-                    menu.AddItem(new GUIContent(path.Split('/').Last()), path == SceneManager.GetActiveScene().path, SwitchScene, path);
-                }
-
+                FillScenesMenu(menu, SwitchScene);
                 menu.ShowAsContext();
             }
         }
@@ -104,16 +103,56 @@ namespace Quario.Tools.Editor
             if (GUILayout.Button(changeSceneContent, GUILayout.Height(23f)))
             {
                 GenericMenu menu = new GenericMenu();
+                FillScenesMenu(menu, AddScene);
+                menu.ShowAsContext();
+            }
+        }
 
+        static void FillScenesMenu(GenericMenu menu, GenericMenu.MenuFunction2 callback)
+        {
+            if (!ShowAllScenesFromProject)
+            {
+                if (EditorBuildSettings.scenes.Length == 0)
+                {
+                    Debug.LogWarning("[Quario:SceneInspector] There is no scenes defined in build settings.");
+                }
+                else foreach (var scene in EditorBuildSettings.scenes)
+                {
+                    var sceneName = System.IO.Path.GetFileNameWithoutExtension(scene.path.Split('/').Last());
+                    menu.AddItem(new GUIContent(sceneName), scene.path == SceneManager.GetActiveScene().path, callback, scene.path);
+                }
+            }
+            else
+            {
                 var scenes = AssetDatabase.FindAssets("t:Scene");
                 foreach (var t in scenes)
                 {
                     var path = AssetDatabase.GUIDToAssetPath(t);
-                    menu.AddItem(new GUIContent(path.Split('/').Last()), path == SceneManager.GetActiveScene().path, AddScene, path);
+                    var sceneName = System.IO.Path.GetFileNameWithoutExtension(path.Split('/').Last());
+                    menu.AddItem(new GUIContent(sceneName), path == SceneManager.GetActiveScene().path, callback, path);
                 }
+            }
+        }
 
+        static void CreateSettingsButton()
+        {
+            GUIContent settingsContent = new GUIContent();
+            settingsContent.image = EditorGUIUtility.IconContent("_Popup").image;
+            settingsContent.tooltip = "Scene inspector settings";
+
+            if (GUILayout.Button(settingsContent, GUILayout.Height(23f)))
+            {
+                GenericMenu menu = new GenericMenu();
+                menu.AddItem(new GUIContent("Show all scenes from project"), ShowAllScenesFromProject, 
+                    ToggleOption, !ShowAllScenesFromProject);
                 menu.ShowAsContext();
             }
+        }
+
+        static void ToggleOption(object option)
+        {
+            ShowAllScenesFromProject = (bool)option;
+            EditorPrefs.SetBool("QuarioToolbox:Inspector:ShowAllScenesFromProject", ShowAllScenesFromProject);
         }
     }
 }
